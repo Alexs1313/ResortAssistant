177f43178b376c748c52f52f8afe51f4dfec83f0
vladikstyle <62757817+vladikstyle@users.noreply.github.com>
2026-07-07 19:37:54 +0300

    news

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './App';
+import AppBootstrap from './AppBootstrap';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => AppBootstrap);
